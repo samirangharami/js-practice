d0bd5c990d8b1ade25c624ec1d848a8dc1c5c562
samirangharami <samirangharami05@gmail.com>
2025-09-19 13:14:44 +0530

    created a program to  print factorials of all numbers from 0 to the given limit

---
+++ factorial/factorial_till_limit.js
@@ -0,0 +1,14 @@
+let startingNumber = 0;
+const limit = 5;
+
+console.log("The limit is:" , limit);
+if (limit < 0) {
+    console.log("The entered limit is negative");
+}
+for (startingNumber ; startingNumber <= limit ; startingNumber++) {
+    let factorial = 1;
+    for (let multiplier = 1; multiplier <= startingNumber; multiplier++) {
+        factorial = factorial *multiplier;
+    }
+    console.log("The factorial of" , startingNumber , ":" , factorial);
+}
