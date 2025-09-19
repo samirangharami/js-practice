1f640047aa5f0c785c1f209acd7d81eb93a65dc7
samirangharami <samirangharami05@gmail.com>
2025-09-19 14:35:29 +0530

    Create division_operator.js

---
+++ arithmetic_operators/division_operator.js
@@ -0,0 +1,10 @@
+let englishMarks = 60;
+let mathsMarks = 88;
+let physicsMarks = 75;
+let chemistryMarks = 40;
+
+let totalMarks =  englishMarks + mathsMarks + physicsMarks + chemistryMarks;
+console.log("Your total marks is" , totalMarks);
+
+let averageMarks = totalMarks / 4 ;
+console.log("Your average marks is" , averageMarks);
