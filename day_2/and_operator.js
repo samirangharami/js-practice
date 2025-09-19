48b27a173eeede818d95cb3533ed5219381acd9f
samirangharami <samirangharami05@gmail.com>
2025-09-19 14:11:02 +0530

    Practiced AND operator

---
+++ day_2/and_operator.js
@@ -0,0 +1,8 @@
+let englishMarks = 60;
+let mathsMarks = 81;
+
+if ((englishMarks < 80) && (mathsMarks < 80)) {
+    console.log("Focus on studies");
+} else {
+   console.log("Great!! Keep it up");
+}
