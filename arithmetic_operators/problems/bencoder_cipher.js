df4ed36fc52fe6aa737d06344e15006a9a624dd8
samirangharami <samirangharami05@gmail.com>
2025-11-03 10:04:56 +0530

    Create bencoder_cipher.js

---
+++ arithmetic_operators/problems/bencoder_cipher.js
@@ -0,0 +1,36 @@
+function bencoderCipherEncoder(data) {
+  return 'i1e';
+}
+
+function formatText(input, actualOutput, expectedOutput) {
+  return `
+  | Inputs  : ${input}
+  | Actual  : ${actualOutput}
+  | Expected: ${expectedOutput}
+  -------`;
+}
+
+function testBencoderCipherEncoder(data, description, expectedOutput) {
+  const actualOutput = bencoderCipherEncoder(data);
+  const isEqual = actualOutput === expectedOutput;
+  const symbol = isEqual ? "✅" : "❌";
+  const headline = `${symbol} ${description}`;
+
+  console.log(headline);
+
+  if (!isEqual) {
+    const input = data;
+    const details = formatText(input, actualOutput, expectedOutput);
+
+    console.log(details);
+  }
+}
+
+function main() {
+  console.log('\nBencoder cipher: encoding');
+  console.log('-'.repeat('Bencoder cipher: encoding'.length), '\n');
+
+  testBencoderCipherEncoder(1, 'one digit integer', 'i1e');
+}
+
+main();
