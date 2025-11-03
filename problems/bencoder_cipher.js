e3575c525154b5b47dbaccd4c52789b13741a889
samirangharami <samirangharami05@gmail.com>
2025-11-03 11:15:38 +0530

    Add support for strings and arrays in encoder
    Enhance bencoderCipherEncoder to handle strings and arrays.

--- problems/bencoder_cipher.js
+++ problems/bencoder_cipher.js
@@ -1,5 +1,22 @@
 function bencoderCipherEncoder(data) {
-  return `i${data}e`;
+  const dataType = typeof (data);
+  switch (dataType) {
+    case 'number':
+      return `i${data}e`;
+    case 'string':
+      return `${data.length}:${data}`;
+    case 'object':
+      return listEncoder(data);
+  }
+}
+
+function listEncoder(data) {
+  let encodedList = '';
+  for (let index = 0; index < data.length; index++) {
+    encodedList += bencoderCipherEncoder(data[index]);
+  }
+
+  return `l${encodedList}e`;
 }
 
 function formatText(input, actualOutput, expectedOutput) {
@@ -34,6 +51,14 @@ function main() {
   testBencoderCipherEncoder(123, 'three digit integer', 'i123e');
   testBencoderCipherEncoder(0, '0 as data', 'i0e');
   testBencoderCipherEncoder(99999, 'five digit integer', 'i99999e');
+  testBencoderCipherEncoder('hi', 'string', '2:hi');
+  testBencoderCipherEncoder('hi hello', 'string with space', '8:hi hello');
+  testBencoderCipherEncoder('', 'empty string', '0:');
+  testBencoderCipherEncoder([1], 'array', 'li1ee');
+  testBencoderCipherEncoder([1, 2, 3], 'array with 3 elements', 'li1ei2ei3ee');
+  testBencoderCipherEncoder([], 'empty array', 'le');
+  testBencoderCipherEncoder([1, 'hello'], 'array with int and str', 'li1e5:helloe');
+  testBencoderCipherEncoder([1, 'hello', [1, 2]], 'array with different data types', 'li1e5:helloli1ei2eee');
 }
 
 main();
