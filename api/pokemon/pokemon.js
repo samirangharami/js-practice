c161822370d2a349572b7af43cef310f8c189534
samirangharami <samirangharami05@gmail.com>
2026-02-10 14:51:30 +0530

    refactors

--- api/pokemon/pokemon.js
+++ api/pokemon/pokemon.js
@@ -1,69 +1,43 @@
+const boldAndUnderline = (text) => "\x1B[4m\x1B[1m" + text + "\x1B[0m";
+
 const fetchPokemon = async (pokeId) => {
   const url = "https://pokeapi.co/api/v2/pokemon/";
   const pokemon = await fetch(url + pokeId).then((x) => x.json());
   return pokemon;
 };
 
-const formatPokeSummary = (pokeSummary) => {
-  const formatted = `
-  
-   ${underline(bold("Id"))}: ${pokeSummary.id}
-   ${underline(bold("Name"))}: ${pokeSummary.name}
-   ${underline(bold("Height"))}: ${pokeSummary.height}
-   ${underline(bold("Weight"))}: ${pokeSummary.weight}
-   ${underline(bold("Types"))}: ${pokeSummary.types}
-   ${underline(bold("Abilities"))}: ${pokeSummary.abilities}
-   ${underline(bold("Stats"))}: ${pokeSummary.baseStats}
-   ${underline(bold("Moves"))}: ${pokeSummary.moves}
-   `;
-
-  return formatted;
+const displaySummary = async (pokeSummary) => {
+  for (const key in pokeSummary) {
+    const detail = `${boldAndUnderline(key)}: ${pokeSummary[key]}\n`;
+    await Deno.stdout.write(new TextEncoder().encode(detail));
+  }
 };
 
-const getAbilities = (pokeDetails) =>
-  pokeDetails.abilities.map((x) => x.ability.name).join(" | ");
-const getMoves = (pokeDetails) =>
-  pokeDetails.moves.map((x) => x.move.name).join(", ");
-const getBaseStats = (pokeDetails) =>
+const extractDetail = (pokeDetails, field, detail, joiner) =>
+  pokeDetails[field].map((x) => x[detail].name).join(joiner);
+
+const extractStats = (pokeDetails) =>
   pokeDetails.stats.map((x) => `${x.stat.name}: ${x.base_stat}`).join(" | ");
-const getTypes = (pokeDetails) =>
-  pokeDetails.types.map((x) => x.type.name).join(" | ");
 
 const getSummary = (pokeDetails) => {
   const id = pokeDetails.id;
   const name = pokeDetails.name;
   const height = pokeDetails.height;
   const weight = pokeDetails.weight;
-  const abilities = getAbilities(pokeDetails);
-  const moves = getMoves(pokeDetails);
-  const baseStats = getBaseStats(pokeDetails);
-  const types = getTypes(pokeDetails);
-  const pokeSummary = {
-    id,
-    name,
-    height,
-    weight,
-    types,
-    abilities,
-    moves,
-    baseStats,
-  };
-  return formatPokeSummary(pokeSummary);
+  const abilities = extractDetail(pokeDetails, "abilities", "ability", "|");
+  const moves = extractDetail(pokeDetails, "moves", "move", ", ");
+  const types = extractDetail(pokeDetails, "types", "type", "|");
+  const stats = extractStats(pokeDetails);
+  const summary = { id, name, height, weight, types, abilities, moves, stats };
+
+  return summary;
 };
 
 const poke = async () => {
   const pokeId = prompt("Enter the name or id:");
   const pokemon = await fetchPokemon(pokeId);
   const pokeSummary = getSummary(pokemon);
-  console.log(pokeSummary);
+  displaySummary(pokeSummary);
 };
 
 poke();
-
-function bold(text) {
-  return "\x1B[1m" + text + "\x1B[0m";
-}
-
-function underline(text) {
-  return "\x1B[4m" + text + "\x1B[0m";
-}
