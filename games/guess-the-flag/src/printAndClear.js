3875e4cfb7c0fa29e59108fda1c4323513c7f35b
samirangharami <samirangharami05@gmail.com>
2026-02-10 16:39:59 +0530

    replaces for loop with spread operator

--- games/guess-the-flag/src/printAndClear.js
+++ games/guess-the-flag/src/printAndClear.js
@@ -4,11 +4,8 @@ export const printFlag = async (flagUrl) => {
   const res = await fetch(flagUrl);
   const buffer = await res.arrayBuffer();
   const bytes = new Uint8Array(buffer);
-  const binary = [];
-  for (const byte of bytes) {
-    binary.push(String.fromCharCode(byte));
-  }
-  const flag = btoa(binary.join(""));
+  const binary = String.fromCharCode(...bytes);
+  const flag = btoa(binary);
   const flagWithEsc = `${ESC}_Gq=2,a=T,f=100;${flag}${ESC}\\`;
   await Deno.stdout.write(new TextEncoder().encode(flagWithEsc + "\n"));
 };
